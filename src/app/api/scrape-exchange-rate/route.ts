eb60ee35fadbf28548deac42112cd4c8e076298b
Mawaziny <amawaziny@gmail.com>
2025-06-15 13:41:10 +0300

    fix currencies

--- src/app/api/scrape-exchange-rate/route.ts
+++ src/app/api/scrape-exchange-rate/route.ts
@@ -48,7 +48,7 @@ export async function GET(req: NextRequest) {
     });
 
     await setDoc(doc(db, "exchangeRates", "current"), {
-      rates,
+      ...rates,
       lastUpdated: new Date().toISOString(),
     });
 
