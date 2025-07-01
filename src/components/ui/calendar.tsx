e7055b2d7b45ce3985dded821f394914632b102e
Mawaziny <amawaziny@gmail.com>
2025-07-01 23:33:20 +0300

    fix icon position

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import DatePicker from "react-multi-date-picker"
-import arabic_ar from "react-date-object/locales/gregorian_ar";
+import locale_ar from "react-date-object/locales/gregorian_ar";
 import "react-multi-date-picker/styles/backgrounds/bg-dark.css"
 import { Calendar as CalenderIcon } from "lucide-react";
 
@@ -26,15 +26,15 @@ function Calendar({
       <DatePicker
         value={value}
         onChange={onChange}
-        locale={locale == "ar" ? arabic_ar : undefined}
+        locale={locale == "ar" ? locale_ar : undefined}
         format={dateFormat === 'dd-MM-yyyy'? "DD-MM-YYYY" : dateFormat}
         maxDate={disableFuture? new Date(): undefined}
         className={"bg-dark"}
         containerClassName="w-full"
-        inputClass="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm w-full pr-10"
+        inputClass="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm w-full ps-10"
         {...(dir === "rtl" ? { rtl: true } : {})}
       />
-      <CalenderIcon className="absolute right-3 top-1/2 -translate-y-1/2 text-sm text-muted-foreground pointer-events-none w-4 h-4" />
+      <CalenderIcon className={`absolute ${dir==='rtl'? "right-3":"left-3"} top-1/2 -translate-y-1/2 text-sm text-muted-foreground pointer-events-none w-4 h-4`} />
     </div>
   )
 }
