5e51748eb57ab6bf2d417a89f160d53ab207aea7
Ahmed El Mawaziny <amawaziny@gmail.com>
2026-02-03 19:45:41 +0000

    todos

--- src/lib/services/dashboard-service.ts
+++ src/lib/services/dashboard-service.ts
@@ -19,13 +19,13 @@ import { dateConverter } from "@/lib/firestore-converters";
  * 1. revisit delete transaction and investment service should subscribe to transaction delete event (start with assuming deleting the last transaction)
  *  a. fire event transaction:deleted with the deleted transaction and investment service needs to listen to it and update the investment average purchase price with the "last transaction"
  *  b. fire event transaction:updated with the updated transaction and investment service needs to listen to it and update the investment average purchase price with the updated transaction
- * 3. Transaction page show calendar to select the month and year to show the expenses for that month and year
- * 4. Debt interest should effect the cash balance automatically (scheduler or same useEffect for handleMaturedDebtInstruments)
- * 5. we could disable some fields in edit mode of investment form
- * 6. refactor investment-form to be each form has its own submit button and logic
- * 7. check if we need context for investment, transaction, expense, income, fixed estimate
- * 8. dashboard is not refreshed automaticly
- * 9. we could list all installements during adding expenses of type credit card
+ * 2. Transaction page show calendar to select the month and year to show the expenses for that month and year
+ * 3. Debt interest should effect the cash balance automatically (scheduler or same useEffect for handleMaturedDebtInstruments)
+ * 4. we could disable some fields in edit mode of investment form
+ * 5. refactor investment-form to be each form has its own submit button and logic
+ * 6. check if we need context for investment, transaction, expense, income, fixed estimate
+ * 7. dashboard is not refreshed automaticly
+ * 8. we could list all installements during adding expenses of type credit card
  */
 
 export class DashboardService {
